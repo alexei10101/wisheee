62d0802d9f00ff19beb849ca4e3d0823b4629f0a
Alexei Derbin <derbinalexei1@gmail.com>
2026-03-01 10:54:54 +0700

    feat: add wishlist page

--- src/features/wishlist/wishlist.page.tsx
+++ src/features/wishlist/wishlist.page.tsx
@@ -1,14 +1,58 @@
-import { UserAuth } from "@/app/auth-context";
+import { wishlistService } from "@/shared/services/wishlist.service";
+import type { Wishlist } from "@/shared/types/wishlist";
 import { Button } from "@/shared/ui/kit/button";
-import { ArrowBigLeft } from "lucide-react";
+import { Spinner } from "@/shared/ui/kit/spinner";
+import { ArrowBigLeft, DiamondPlus, Forward, Plus } from "lucide-react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 
 const WishlistPage = () => {
   const navigate = useNavigate();
-  const { profile } = UserAuth();
   const { id } = useParams<{ id: string }>();
-  const wishlist = profile?.wishlists.find((wishlist) => wishlist.id === id);
-  if (!wishlist) return;
+  const [wishlist, setWishlist] = useState<Wishlist | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  // Get wishlist
+  useEffect(() => {
+    if (!id) return;
+
+    let isMounted = true;
+
+    const loadWishlist = async () => {
+      setLoading(true);
+      setError(null);
+
+      const { result, error } = await wishlistService.read(id);
+
+      if (!isMounted) return;
+
+      if (error) {
+        setError(error);
+        setLoading(false);
+        return;
+      }
+
+      setWishlist(result);
+      setLoading(false);
+    };
+
+    loadWishlist();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
+  if (loading)
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-background">
+        <Spinner />
+      </div>
+    );
+  if (error) return <div className="pt-25 px-4 text-red-500">{error}</div>;
+  if (!wishlist) return <div className="pt-25 px-4">Вишлист не найден</div>;
+
+  const handleCreate = () => {};
 
   return (
     <section className="pt-25 px-4">
@@ -20,8 +64,18 @@ const WishlistPage = () => {
         </div>
       </div>
 
-      <div>Добавить подарок</div>
-      <div>Поделиться вишлистом</div>
+      <div className="flex gap-6 flex-wrap items-baseline">
+        <h2 className="text-3xl">Подарки вишлиста</h2>
+        <Button variant="ghost" className="hover:bg-gray-300" onClick={handleCreate}>
+          <DiamondPlus />
+        </Button>
+      </div>
+      <div>
+        Добавить подарок <Plus />
+      </div>
+      <div>
+        <Forward />
+      </div>
 
       <div>Настроить порядок: по дате, по цене, по названию</div>
       <div>Добавить фильтры: все подарки, забронированные, незабронированные, исполненные, неисполненные</div>
